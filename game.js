c4fccf09f49012072dd39e512db850d22bd8de7c
Felipe de Paula <emilcyfelipe@gmail.com>
2021-05-07 23:35:01 -0300

    Completed

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 
 let cardsToMatch=[];
 
+//Verifica as cartas clicadas
 function match(){
     if(cardsToMatch[0].children[1].children[0].className==cardsToMatch[1].children[1].children[0].className){
         return(true);
--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let deck = document.querySelector(".container");
+//icones disponiveis
 let dataCards=["<i class='fas fa-dog'></i>","<i class='fas fa-dove'></i>","<i class='fas fa-fish'></i>","<i class='fas fa-dragon'></i>","<i class='fas fa-horse'></i>","<i class='fas fa-spider'></i>","<i class='fas fa-crow'></i>"];
-let nCards=14;
-// lista de simbolos da partida
+let nCards=0;
+
+//Usuário define o tamanho do deck
+let buttonsSize = document.querySelectorAll(".b-size");
+buttonsSize.forEach((button)=>{
+    button.addEventListener(("click"),()=>{
+        nCards=parseInt(event.target.id);
+        console.log(nCards);
+        cards(nCards);
+        document.querySelector(".setSize").innerHTML="";
+    });
+});
+
+// escolha aleatoria de simbolos da partida
 function chooseCards(metadecartas){
     let choseCard =[];
     for(i=0;i<metadecartas;i++){
@@ -11,7 +24,7 @@ function chooseCards(metadecartas){
     }
     return(choseCard);
 }
-//  console.log(chooseCards(5));
+
 
 //funçao total de cartas no deck
 function deckCards(){
@@ -42,44 +55,44 @@ function shuffle(){
 // função adiciona cartas ao tabuleiro
 function cards(nCards){
     let deckShuffle = shuffle();
-    // console.log(deckShuffle);
+    
     for(i=0;i<nCards;i++){
         deck.innerHTML+="<div class='card'><div class='c-backFace'><i class='fas fa-feather'></i></div><div class='c-frontFace'>"+deckShuffle[i]+"</div></div>";
     }
+    // //adicionar o evento clique nas cartas
+    let cardEvent = document.querySelectorAll('.card');
+    cardEvent.forEach((card) =>{
+    card.addEventListener(("click"), flipIn);
+});
 }
-cards(nCards);
 
-//adicionar o evento clique nas cartas
-let cardEvent = document.querySelectorAll('.card');
 
-cardEvent.forEach((card) =>{
-    card.addEventListener(("click"),()=>{
-        
-        cardsToMatch.push(event.target);
-        event.target.classList.add("flip");
-        if(cardsToMatch.length==2){
-            let matches = match();
-            if(matches==true){
-                // cardsToMatch[0].removeEventListener("click");
-                // cardsToMatch[1].removeEventListener("click");
-                cardsToMatch=[];
-
-            }else{
-                setTimeout(FlipOut,1000);
+let winner =0;
+//gira carta por partida
+function flipIn(){
+    cardsToMatch.push(event.target);
+    event.target.classList.add("flip");
+    if(cardsToMatch.length==2){
+        let matches = match();
+        if(matches==true){
+            cardsToMatch[0].removeEventListener(("click"),flipIn);
+            cardsToMatch[1].removeEventListener(("click"),flipIn);
+            cardsToMatch=[];
+            winner++
+            if(winner==nCards/2){
+                document.querySelector(".container").innerHTML+="<div class='won'>Congratulations, you won!!</div>"
             }
-        
+
         }else{
+            var cardsToFlip = cardsToMatch;
+            cardsToMatch=[];
+            console.log(cardsToFlip);
+            setTimeout(()=>{cardsToFlip[0].classList.remove("flip");
+            cardsToFlip[1].classList.remove("flip");
+        },1000);
 
         }
-        
-
-        // let click1 = 
-    });
-});
-
-function FlipOut(){
-    cardsToMatch[0].classList.remove("flip")
-    cardsToMatch[1].classList.remove("flip")
-    cardsToMatch=[];
+    
+    }
 }
 
